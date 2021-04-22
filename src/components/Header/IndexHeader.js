ecee85a8fe5a3f0b312b6389581c5aab41a775d8
Pranav Bakre <pranavbakre@yahoo.in>
2021-04-22 19:02:24 +0530

    Index Header and basic css for links, logo and background
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

---
+++ src/components/Header/IndexHeader.js
@@ -0,0 +1,29 @@
+import { NavLink, Link } from "react-router-dom";
+import * as styles from "./header.module.css";
+import logo from "images/vppr-darktext.svg";
+import logoWhite from "images/vppr-whitetext.svg";
+import { useThemeContext } from "context/theme-context";
+function IndexHeader() {
+  const { theme } = useThemeContext();
+  return (
+    <nav className={`header ${styles.header}`}>
+      <Link to="/" className="link">
+        <img
+          src={theme === "dark" ? logoWhite : logo}
+          className="logo"
+          alt="VPPR"
+        />
+      </Link>
+      <div className={styles.headerItem}>
+        <NavLink to="/login" className="link">
+          <h3>Login</h3>
+        </NavLink>
+        <NavLink to="/register" className="link">
+          <h3>Register</h3>
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default IndexHeader;
